fe5840f477be2ab2b0aca5ecdee1d8b7c663da31
Edgar Mcochieng <edgarmcochieng@gmail.com>
2026-01-17 14:16:05 -0600

    Add __init__.py to tests directory for package initialization

---
+++ tests/__init__.py
@@ -0,0 +1,11 @@
+"""
+Test suite for AzureSearch FileShare Indexer
+
+Author: Edgar McOchieng
+"""
+
+import os
+import sys
+
+# Add parent directory to path for imports
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
